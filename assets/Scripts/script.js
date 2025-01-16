61da3d5d11c8320d41c0ad0a14c3ea58261baff8
Aman Kothari <amankothari.developer@gmail.com>
2025-01-17 03:15:20 +0530

    Footer and Other Fixes Done

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -1,3 +1,4 @@
+// Slider For Popular Destinations ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 $(".dest-card-slider").slick({
   infinite: false,
   speed: 300,
@@ -7,7 +8,10 @@ $(".dest-card-slider").slick({
   prevArrow: $(".prev"),
   nextArrow: $(".next"),
 });
+
+// Wait For Dom Parsing ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 window.addEventListener("DOMContentLoaded", () => {
+  // Theme Dark and Light mode switcher with localstorage sync ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   let themeMode = localStorage.getItem("theme-mode");
 
   if (themeMode == "dark") {
@@ -25,11 +29,23 @@ window.addEventListener("DOMContentLoaded", () => {
       localStorage.setItem("theme-mode", "dark");
     }
   });
+  // Theme Dark and Light mode switcher with localstorage sync -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  let themeColor = localStorage.getItem("theme-color");
 
+  if (themeColor != null) {
+    document.getElementById("theme-color").value = themeColor;
+    $("html, .neg-theme").css("--theme-color", themeColor);
+  }
+
+  $("#theme-color").change((e) => {
+    let color = e.currentTarget.value;
+    localStorage.setItem("theme-color", color);
+    $("html, .neg-theme").css("--theme-color", color);
+  });
+
+  // Newsletter data submition and form submition alert for user ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   $(".newsletter-form").submit((e) => {
     e.preventDefault();
-    alert(
-      `Updates will notified you on ${e.currentTarget[0].value}`
-    );
+    alert(`Updates will notified you on ${e.currentTarget[0].value}`);
   });
 });
